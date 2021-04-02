7cc6a15ad993edf2e8d58834422e6d53b899e77c
Choi-jinwoo <chlwlsdn0828@gmail.com>
2021-04-02 22:23:33 +0900

    Add: SelectServiceCard 컴포넌트 UI 및 로직 제작

---
+++ src/components/home/select-service-card/index.tsx
@@ -0,0 +1,40 @@
+import React, { useCallback } from 'react';
+import { Text } from 'components/common/text';
+
+import './index.scss';
+import { useHistory } from 'react-router';
+
+type Props = {
+  title: string;
+  subTitle: string;
+  image: string;
+  targetPath: string;
+}
+
+export const SelectServiceCard = ({
+  title,
+  subTitle,
+  image,
+  targetPath,
+}: Props) => {
+
+  const history = useHistory();
+
+  const movePage = useCallback(() => {
+    history.push(targetPath);
+  }, [history, targetPath]);
+
+  return (
+    <div
+      className='selectServiceCard'
+      onClick={movePage} >
+      <Text
+        className='selectServiceCard-title'
+        size='big'>{title}</Text>
+      <Text
+        className='selectServiceCard-subTitle'
+      >{subTitle}</Text>
+      <img src={image} alt="" />
+    </div>
+  )
+}
