7e5b6c57375d7ef18c3e15133d358ab1f49e6f87
Choi-jinwoo <chlwlsdn0828@gmail.com>
2021-04-03 20:30:11 +0900

    Fix: PageTemplate을 HomePage에 적용

--- src/pages/home.page.tsx
+++ src/pages/home.page.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Home } from 'components/home';
+import { PageTemplate } from 'components/common/page-template';
 
 export const HomePage = () => {
   return (
-    <div className="homePage">
+    <PageTemplate>
       <Home />
-    </div>
+    </PageTemplate>
   )
 }
